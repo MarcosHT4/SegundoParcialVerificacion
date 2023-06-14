943ba62e768696b7e7c8cdc31329a0c98395a479
Marcos Simon <marcosht503@gmail.com>
2023-06-14 16:18:45 -0400

    Dependencias iniciales

---
+++ src/test/java/api/utils/Properties.java
@@ -0,0 +1,9 @@
+package api.utils;
+
+public class Properties {
+
+    public static String host = "https://todo.ly/";
+    public static String user = "marcos@vpsi.com";
+    public static String pwd = "12345";
+
+}
